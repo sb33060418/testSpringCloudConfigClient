6bbf123654e6cd2c49b2f1b0d7bd381d658925d8
sunbin <sunbin@windows10.microdone.cn>
2019-05-25 17:55:04 +0800

    spring cloud config client

--- src/main/java/com/sunbin/test/spring/cloud/config/TestSpringCloudConfigClientApplication.java
+++ src/main/java/com/sunbin/test/spring/cloud/config/TestSpringCloudConfigClientApplication.java
@@ -2,14 +2,12 @@ package com.sunbin.test.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class TestSpringCloudConfigServerApplication {
+public class TestSpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestSpringCloudConfigServerApplication.class, args);
+		SpringApplication.run(TestSpringCloudConfigClientApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/sunbin/test/spring/cloud/config/controller/ConfigController.java
@@ -0,0 +1,19 @@
+package com.sunbin.test.spring.cloud.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+	
+	@Value("${name}")
+	private String name;
+
+	@RequestMapping("/get")
+	public String getMessage() {
+		return "get config: name = "+ name;
+	}
+
+}
