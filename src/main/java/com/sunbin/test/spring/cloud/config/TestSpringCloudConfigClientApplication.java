032e244bf78c0c4809b80e01e58dcb5769d09e31
sunbin <sunbin@windows10.microdone.cn>
2019-05-27 17:42:24 +0800

    修改为使用eureka注册configserver

--- src/main/java/com/sunbin/test/spring/cloud/config/TestSpringCloudConfigClientApplication.java
+++ src/main/java/com/sunbin/test/spring/cloud/config/TestSpringCloudConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.sunbin.test.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // 开启发现服务功能
 @SpringBootApplication
 public class TestSpringCloudConfigClientApplication {
 
